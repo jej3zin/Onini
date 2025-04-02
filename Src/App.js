ab00b0e4e8c5d1e18217ed0d1998e39afb44cd20
jej3zin <127148233+jej3zin@users.noreply.github.com>
2025-04-02 09:33:22 -0300

    Add files via upload

---
+++ Src/App.js
@@ -0,0 +1,14 @@
+/* /* GUIA *
+let a = parseInt(prompt('Digite um número entre 0 a 10.'));
+let b = parseInt(prompt('Digite um outro número entre 0 a 10.'));
+let c = parseInt(prompt('Digite mais um número entre 0 a 10.'));
+
+if (a <= 10 && b <= 10 && c <= 10 && a >= 0 && b >= 0 && c >= 0 && isNaN) {
+}
+if (result <= c && isNaN) {
+  let result = a + b;
+  alert('A soma de A + B é maior que C.');
+} else {
+  alert('A soma de A + B é menor que C.');
+}
+ */
